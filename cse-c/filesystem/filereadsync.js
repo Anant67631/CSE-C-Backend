4726f7c177a973b62879d7aa2bbedebf393f2adf
Anant67631 <anantsagar369@gmail.com>
2025-02-11 10:13:10 +0530

    updated data 11/02/2025

--- cse-c/filesystem/filereadsync.js
+++ cse-c/filesystem/filereadsync.js
@@ -1,3 +1,16 @@
 const fs=require("fs");
-const data=fs.readFileSync("./data.txt");
+//fs.writeFileSync("./data1.txt","HELLO ABES","utf-8");
+//fs.writeFileSync("./data1.txt","HELLO ABESec","utf-8");
+//fs.appendFileSync("./data1.txt"," students","utf-8");
+//fs.renameSync("./data2.txt","./data3.txt");
+// fs.unlinkSync("./data3.txt");
+// const data=fs.readFileSync("./data.txt","utf-8");
+// console.log(data.toString())
+// console.log(data)
+const data=fs.readFileSync("./data.txt","utf-8");
 console.log(data)
+if (data.match("H")){
+    console.log("File contains 'H'");
+    const newdata=data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
